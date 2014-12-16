0a03dd16adba0d6fd34e996b95e10b0af74c8146
Andre Weissflog <floooh@gmail.com>
2014-12-16 20:36:36 +0100

    Added a dep1 test lib and app

---
+++ src/dep1/dep1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_dep1() {
+    printf("Hello from dep1\n");
+}
+
---
+++ src/test/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "dep1/dep1.h"
+
+int main() {
+    print_dep1();
+    return 0;
+}
+
+
